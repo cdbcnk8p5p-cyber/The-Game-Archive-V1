eb47073768fe1da95c43448e6d909abd56caa9ac
cdbcnk8p5p-cyber <cdbcnk8p5p@privaterelay.appleid.com>
2026-08-13 23:15:39 +0100

    Fix hidden legacy filter default

--- archive-v2-loader.js
+++ archive-v2-loader.js
@@ -5,7 +5,7 @@ function ensureLegacyStatusControl(){
  const select=document.createElement('select');
  select.id='gameStatusFilter';
  select.hidden=true;
- select.innerHTML='<option value="All">All statuses</option><option value="Beaten">Beaten</option><option value="Completed">Completed</option><option value="Incomplete">Incomplete</option><option value="Not Started">Not Started</option>';
+ select.innerHTML='<option value="" selected>All statuses</option><option value="Beaten">Beaten</option><option value="Completed">Completed</option><option value="Incomplete">Incomplete</option><option value="Not Started">Not Started</option>';
  document.body.appendChild(select);
 }
 ensureLegacyStatusControl();
