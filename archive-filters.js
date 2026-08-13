7282c923f523db742fd3fc531e89fc6ab9793664
cdbcnk8p5p-cyber <cdbcnk8p5p@privaterelay.appleid.com>
2026-08-13 23:59:54 +0100

    Add Sega archive filter options

--- archive-filters.js
+++ archive-filters.js
@@ -4,6 +4,7 @@ const FILTER_OPTIONS={
  Nintendo:['Nintendo 3DS','Nintendo DS','Nintendo Switch','Wii','Wii U'],
  PC:['Epic Games Store','GOG','Microsoft Store','Rockstar Games Launcher','Steam'],
  PlayStation:['PlayStation','PlayStation 2','PlayStation 3','PlayStation 4','PlayStation 5','PlayStation Portable','PlayStation Vita'],
+ Sega:['Dreamcast','Game Gear','Mega Drive','Master System','Saturn'],
  Xbox:['Xbox','Xbox 360','Xbox One','Xbox Series X/S']
 };
 function optionList(type,games){
@@ -23,6 +24,7 @@ window.GAMING_ARCHIVE_FILTERS={
  matches(game,type,option){
   if(!type)return true;
   if(type==='status')return !option||game.status===option;
+  if(type==='PC')return game.family==='PC'&&(!option||[game.storefront,game.launcher,game.platform].filter(Boolean).includes(option));
   if(Object.prototype.hasOwnProperty.call(FILTER_OPTIONS,type))return game.family===type&&(!option||game.platform===option);
   return true;
  }
